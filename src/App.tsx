c1227a884fe6d9d32edc4b0cdd6243f5e256668d
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-09-27 23:14:41 -0300

    :lipstick: feat: pequenas estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import './App.css'
+import './scss/App.css'
 import QueryBox from './components/QueryBox'
 import Button from './components/Buttons/Buttons.tsx'
+import { styled } from 'styled-components'
 
 
 const App = () => {
+
+  const Container__article = styled.article`
+    text-align: center;
+    input,button{
+      padding: 1rem 2rem;
+    }
+  `
+
   return (
     <>
       <div className="container">
-        <h1>Consulte todas as informações do seu CNPJ de forma gratuíta</h1>
-        <article className='container__article'>
+        <div className="container__text">
+          <h1>Consulte todas as informações do seu CNPJ de forma gratuíta</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dicta, consequuntur atque accusantium temporibus dolorum recusandae alias enim maxime eius sequi ipsa, dignissimos, ducimus vitae labore! Accusantium sint magnam modi.</p>
+        </div>
+        <Container__article>
           <QueryBox />
           <Button />
-        </article>
+        </Container__article>
         <table className="container__table">
           <tr>
             <td>info 1</td>
